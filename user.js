c98dbb67e59e39c8d35e687e3c65b9c7d551b85b
Cris <CrisBRM@users.noreply.github.com>
2015-06-08 14:43:21 +0100

    browser.newtab.preload
    Self-explanatory; Set to true if it's locally hosted (in which case, there is no actual downside).

--- user.js
+++ user.js
@@ -27,6 +27,7 @@ user_pref("browser.migration.version", 27);
 user_pref("browser.newtab.url", "about:blank");
 user_pref("browser.newtabpage.enhanced", false);
 user_pref("browser.newtabpage.directory.ping", "");
+user_pref("browser.newtab.preload", false); //comment out if it's a local page
 user_pref("browser.safebrowsing.appRepURL", "");
 user_pref("browser.safebrowsing.downloads.enabled", false);
 user_pref("browser.safebrowsing.downloads.remote.enabled", false);
